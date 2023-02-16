3d3a3827996dbe4100d24e6b5eda0784aa50ff2c
Silvian Dragan <silvian.dragan@gmail.com>
2023-02-16 08:00:52 +0000

    implemented catalogue

--- app/api/routers/catalogue.py
+++ app/api/routers/catalogue.py
@@ -1,9 +1,12 @@
+import grpc
 import logging
 from typing import List
 
 from fastapi import APIRouter, HTTPException, status
 
 from ..models.catalogue import Item, ItemDetails
+from ..pb2 import catalogue_pb2 as catalogue
+from ..pb2 import catalogue_pb2_grpc as catalogue_grpc
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +22,11 @@ router = APIRouter()
 async def list_all_catalogue_items() -> List[Item]:
     """Returns a list of all the available catalogue items."""
     items = []
+    with grpc.insecure_channel('10.0.8.146:50051') as channel:
+        stub = catalogue_grpc.CatalogueStub(channel)
+        response = stub.listCatalogueItems(catalogue.ItemsRequest(itemsLimit=1))
+        print(f"Response: {response.items}")
+        items = response.items
     if not items:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="No catalogue items currently available."
@@ -36,6 +44,18 @@ async def list_all_catalogue_items() -> List[Item]:
 async def get_catalogue_item_details(id: str) -> ItemDetails:
     """Returns catalogue item details and reviews."""
     item = None
+    with grpc.insecure_channel('10.0.8.146:50051') as channel:
+        stub = catalogue_grpc.CatalogueStub(channel)
+        response = stub.getItemDetails(catalogue.ItemDetailsRequest(itemId=id, reviewsLimit=10))
+        print(f"Response: {response}")
+        item = ItemDetails(
+            id=response.id,
+            name=response.name,
+            image=response.image,
+            rating=response.rating,
+            description=response.description,
+            reviews=[],
+        )
     if not item:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
